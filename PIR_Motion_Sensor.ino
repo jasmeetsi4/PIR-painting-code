d7b49da5228c9794ea7e5db037e97e3c6e5e247b
jasmeetsi4 <38372611+jasmeetsi4@users.noreply.github.com>
2019-09-16 14:30:06 +1000

    Update PIR_Motion_Sensor.ino

--- PIR_Motion_Sensor.ino
+++ PIR_Motion_Sensor.ino
@@ -1,16 +1,24 @@
+int LED=13;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(6, INPUT);
+  pinMode(7,INPUT);
+  pinMode(8,INPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(6) == HIGH) {
+  if(digitalRead(6)==HIGH||digitalRead(7)==HIGH||digitalRead(8)==HIGH)
+  {
     Serial.println("Active");
+    digitalWrite(LED,HIGH);
   }
-  else {
+  else
+  {
     Serial.println("Inactive");
+    digitalWrite(LED,LOW);
   }
   delay(1000);
 }
